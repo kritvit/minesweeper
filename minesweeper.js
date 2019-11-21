7d1a28754946dda428677fcbcd6e9053d4d2128b
Andreas Viklund <github@kritvit.se>
2019-11-21 20:58:15 +0100

    Moved game logic to separate file

--- minesweeper.js
+++ minesweeper.js
@@ -2,28 +2,49 @@
 
 	'use strict';
 
-	const minesweeperTmpl = function () {
+	function minesweeperTmpl () {
+
+		// https://unicode.org/emoji/charts/full-emoji-list.html
+		// https://www.fileformat.info/info/unicode/char/1F973/index.htm
 
 		const template = `
 
 			<div class="minesweeper-controls">
 				<br><br>
 				<p>- SETTINGS -</p>
-				<button type="button" class="controls-level" data-level="0">easy</button>
-				<button type="button" class="controls-level" data-level="1">medium</button>
-				<button type="button" class="controls-level"  data-level="2">hard</button>
+				<button type="button" class="controls-level -is-current" data-level="easy">easy</button>
+				<button type="button" class="controls-level" data-level="medium">medium</button>
+				<button type="button" class="controls-level"  data-level="hard">hard</button>
 				<br><br>
 				<p>- INFO -</p>
-				<p>Start game by clicking on a square.</p>
-				<p>To flag a square, press space bar while hovering.</p>
+				<p>
+					Start game by clicking on a square.
+				</p>
+				<p>
+					Hover item and press space to flag item.
+				</p>
+				<p>
+					"space"
+					<br>
+					flag hovered item
+				</p>
+				<p>
+					"R"<br>
+					reset game
+				</p>
+				<p>
+					"C"<br>
+					clear hovered item
+				</p>
 				<br><br>
-				<button type="button" class="expand-toggle" title="Configure Game">Settings / Info</button>
+				<button type="button" class="minesweeper-expand-toggle" title="Configure Game">Settings / Info &#x2699;</button>
 			</div>
 
 			<div class="minesweeper-status">
-				<button type="button" class="reset" title="Reset Game"></button>
-				<div class="flagged">Hover item and press space to flag item</div>
-				<div class="status"></div>
+				<button type="button" class="minesweeper-reset" title="Reset Game">&#x267b;</button>
+				<div class="item">&#x1f6a9; <span class="marked"></span></div>
+				<div class="item">Cleared: <span class="progress"></span></div>
+				<div class="item"><div class="status"></div></div>
 			</div>
 
 			<div class="minesweeper-board"></div>
@@ -36,164 +57,89 @@
 
 	class Item {
 
-		constructor (game, row, col) {
-
-			const item = this;
+		constructor (game, props) {
 
 			const node = document.createElement('div');
 
+			this.node = node;
+
+			this.props = props;
+
+			this.game = game;
+
+			this.state = game.state;
+
 			node.classList.add('minesweeper-item');
 
-			node.setAttribute('style', 'flex-basis: '+(100/game.boardSize[0])+'%;');
-
-			node.innerHTML = 'O';
+			node.setAttribute('style', 'flex-basis: '+(100/this.state.minesweeperEngine.cols)+'%;');
 
 			node.addEventListener('click', () => {
-				item.clear('click');
+
+				this.state.minesweeperEngine = this.state.minesweeperEngine.clear(props.id);
+
+				this.update();
+
 			});
 
 			node.addEventListener('mouseover', () => {
-				game.currentItem = item;
+				this.state.currentItem = this;
 			});
-
-			item.isHovered 	= false;
-
-			item.node 		= node;
-
-			item.row 		= row;
-
-			item.col 		= col;
-
-			item.isMine 	= game.mineMap.indexOf(row+','+col) > -1;
-
-			item.game 		= game;
 
 		}
 
-		forEachNeighbour (callback) {
+		update () {
 
-			const item = this;
+			this.state.minesweeperEngine.items.forEach(item => {
 
-			const neighbours = [
-				[0,-1], 	// before
-				[0,1], 		// after
-				[-1,-1], 	// above before
-				[-1,0], 	// above
-				[-1,1], 	// above after
-				[1,-1], 	// below before
-				[1,0], 		// below
-				[1,1], 		// below after
-			];
+				const node = this.state.items[item.id].node;
 
-			neighbours.forEach((neighbour) => {
+				this.game.toggleStateClassname(node, item.status);
 
-				const row = item.row+neighbour[0];
-				const col = item.col+neighbour[1];
+				switch (item.status) {
 
-				if (item.game.board[row] && item.game.board[row][col]) {
+				case 'cleared':
 
-					callback(item.game.board[row][col]);
+					node.innerHTML = item.border || '';
+					break;
+
+				case 'marked':
+
+					node.innerHTML = '&#x1f6a9;';
+					break;
+
+				case 'missmarked':
+
+					node.innerHTML = '&#x1f6ab;';
+					break;
+
+				case 'triggered':
+
+					node.innerHTML = '&#x1f4a5;';
+					break;
+
+				case 'exposed':
+
+					node.innerHTML = '&#x1f4a3;';
+					break;
+
+				default:
+
+					node.innerHTML = '';
 
 				}
 
 			});
 
-		}
+			this.game.render();
 
-		getNumber () {
-
-			let number = 0;
-
-			this.forEachNeighbour((neighbour) => {
-				if (neighbour.isMine) {
-					number++;
-				}
-			});
-
-			return number;
 		}
 
 		flag () {
 
-			const item = this;
-			const game = item.game;
+			this.state.minesweeperEngine = this.state.minesweeperEngine.mark(this.props.id);
 
-			if (!item.isCleared && item.game.state.isOn) {
+			this.update();
 
-				if (item.isFlagged) {
-
-					game.state.flagged 	= game.state.flagged-1;
-					item.isFlagged 		= false;
-
-					item.node.classList.remove('-flagged');
-
-				} else {
-
-					game.state.flagged 	= game.state.flagged+1;
-					item.isFlagged 		= true;
-
-					item.node.classList.add('-flagged');
-
-				}
-
-				game.render();
-
-			}
-
-		}
-
-		clear (event) {
-
-			const item = this;
-
-			if (event === 'click' && !item.game.state.isOn) {
-				item.game.start();
-			}
-
-			if (item.isMine && !item.isFlagged || event === 'game-over') {
-
-				item.isCleared = true;
-				item.node.innerHTML = 'X';
-				item.node.classList.add('-cleared', '-exploded');
-
-				if (event === 'click') {
-					item.game.state.isLost = true;
-					item.game.render();
-				}
-
-			} else if (!item.isCleared && !item.isFlagged) {
-
-				const board 	= item.game.board;
-				const number 	= item.getNumber();
-				const row 		= item.row;
-				const col 		= item.col;
-
-				if ('number' === typeof row && 'number' === typeof col) {
-					if (board[row] && board[row][col]) {
-
-						item.node.classList.add('-cleared');
-						item.isCleared = true;
-						item.game.state.cleared = item.game.state.cleared++;
-
-						if (number > 0) {
-							item.node.innerHTML = number;
-							item.node.classList.add('-border');
-						} else {
-
-							setTimeout(() => {
-								item.forEachNeighbour((neighbour) => {
-									neighbour.clear();
-								});
-							}, 20);
-							
-
-						}
-
-						item.game.render();
-
-					}
-				}
-			}
 		}
 
 	}
@@ -202,283 +148,199 @@
 
 		constructor (wrapperNode) {
 
-			const game = this;
-
-			const boardSizes = {
-				0: [10, 10],
-				1: [20, 20],
-				2: [30, 30]
+			this.state = {
+				currentItem: null,
+				minesweeperEngine: window.minesweeperEngine(),
+				level: 'easy',
+				levels: {
+					easy: 	[10, 10, 10],
+					medium: [20, 20, 50],
+					hard: 	[30, 30, 100]
+				},
+				items: {},
+				classnames: []
 			};
 
-			const minesAmount = {
-				0: 10,
-				1: 50,
-				2: 100
-			};
-
-			game.state = {};
-
-			game.prevState = {};
-
-			game.node = {
+			this.node = {
 				board: 		wrapperNode.querySelector('.minesweeper-board'),
 				status: 	wrapperNode.querySelector('.minesweeper-status'),
 				controls: 	wrapperNode.querySelector('.minesweeper-controls')
 			};
 
-			game.level 			= 1;
-			game.boardMap 		= [];
-			game.boardSize 		= boardSizes[game.level];
-			game.minesAmount 	= minesAmount[game.level];
+			this.initControls();
+			this.initBoard();
 
-			// Map of board coordinates.
-			for (let rowIndex = 0; rowIndex < game.boardSize[1]; rowIndex++) {
-				for (let colIndex = 0; colIndex < game.boardSize[0]; colIndex++) {
-					game.boardMap.push(rowIndex+','+colIndex);
+		}
+
+		initBoard () {
+
+			this.node.board.innerHTML = '';
+			this.state.items = [];
+			this.state.currentItem = null;
+
+			this.state.minesweeperEngine.eachItem(item => {
+
+				const boardItem = new Item(this, item);
+
+				if (!this.state.items[item.id]) {
+
+					this.state.items[item.id] = boardItem;
+
 				}
-			}
 
-			game.initStatus();
-			game.initControls();
-			game.initBoard();
+				this.node.board.append(boardItem.node);
+
+			});
+
+			this.render();
+
+		}
+
+		initControls () {
+
+
+			document.querySelector('.minesweeper').addEventListener('click', (event) => {
+
+				const target = event.target.classList;
+				const controls = this.node.controls.classList;
+
+				if (target.contains('minesweeper-item')) {
+
+					controls.remove('-expanded');
+
+				}
+
+				if (target.contains('minesweeper-expand-toggle')) {
+
+					controls.toggle('-expanded');
+
+				}
+
+				if (target.contains('minesweeper-reset')) {
+
+					this.reset();
+
+				}
+
+			});
+
+			this.node.controls.querySelectorAll('.controls-level').forEach((node) => {
+
+				node.addEventListener('click', () => {
+
+					const level = node.dataset.level;
+
+					this.reset(level);
+
+				});
+
+			});
 
 			document.addEventListener('keydown', (event) => {
 
 				switch (event.keyCode) {
 
-					case 32:
-						game.currentItem.flag();
+					case 32: // space
+						this.state.currentItem.flag();
 						break;
-
-				}
-
-			});
-		}
-
-		initBoard (config) {
-			
-			window.console.log(this);
-
-			const game 					= this;
-
-			config 						= config || {};
-
-			game.board 					= {};
-			game.boardArray 			= [];
-			game.mineMap 				= [];
-			game.currentItem 			= null;
-			game.node.board.innerHTML 	= '';
-
-			game.state = {
-				flagged: 	0,
-				cleared: 	0,
-				isOn: 		false,
-				isWon: 		false,
-				isLost: 	false
-			};
-
-			// Map of mine coordinates.
-			while (game.mineMap.length < game.minesAmount) {
-
-				const randomIndex = Math.floor((Math.random() * game.boardMap.length));
-
-				if (game.boardMap[randomIndex] && game.mineMap.indexOf(game.boardMap[randomIndex]) === -1) {
-					game.mineMap.push(game.boardMap[randomIndex]);
-				}
-			}
-
-			// Create/append board items.
-
-			game.boardMap.forEach((coordinates) => {
-
-				const rowCol 			= coordinates.split(',');
-				const row 				= parseInt(rowCol[0]);	
-				const col 				= parseInt(rowCol[1]);
-				const item 				= new Item(game, row, col);
-
-				game.board[row] 		= game.board[row] || {};
-				game.board[row][col] 	= item;
-
-				game.boardArray.push(item);
-
-				game.node.board.append(item.node);
-
-			});
-
-			game.render();
-
-		}
-
-		initControls () {
-			this.node.controls.querySelector('.expand-toggle').addEventListener('click', () => {
-				this.node.controls.classList.toggle('-expanded');
-			});
-
-			this.node.controls.querySelectorAll('.controls-level').forEach((node) => {
-				node.addEventListener('click', () => {
-
-					const level = parseInt(node.dataset.level);
-
-					if (typeof level === 'number') {
-
-						this.changeLevel(level);
-
-					}
-
-				});
-			});
-		}
-
-		initStatus () {
-			this.node.status.querySelector('.reset').addEventListener('click', () => {
-				this.restart();
-			});
-		}
-
-		changeLevel (level) {
-
-			const boardSizes = {
-				0: [10, 10],
-				1: [20, 20],
-				2: [30, 30]
-			};
-
-			const minesAmount = {
-				0: 10,
-				1: 50,
-				2: 100
-			};
-
-			this.boardMap 		= [];
-			this.boardSize 		= boardSizes[level];
-			this.minesAmount 	= minesAmount[level];
-
-			// Map of board coordinates.
-			for (let rowIndex = 0; rowIndex < this.boardSize[1]; rowIndex++) {
-				for (let colIndex = 0; colIndex < this.boardSize[0]; colIndex++) {
-					this.boardMap.push(rowIndex+','+colIndex);
-				}
-			}
-
-			this.initBoard({level: level});
-
-		}
-
-		forEachItem (callback) {
-			this.boardArray.forEach((item) => {
-				callback(item);
-			});
-		}
-
-		forEachMine (callback) {
-			this.mineMap.forEach((coordinates) => {
-
-				const rowCol 	= coordinates.split(',');
-				const row 		= parseInt(rowCol[0]);
-				const col 		= parseInt(rowCol[1]);
-
-				callback(this.board[row][col]);
-
-			});
-		}
-
-		selectNode (selector) {
-			return this.node.status.querySelector(selector);
-		}
-
-		evaluate () {
-
-			let itemsLeft = 0;
-
-			this.forEachItem((item) => {
-
-				if (!item.isMine && !item.isCleared) {
-					itemsLeft++;
+					case 67: // c
+						this.state.minesweeperEngine = this.state.minesweeperEngine.clear(this.state.currentItem.props.id);
+						this.state.currentItem.update();
+						break;
+					case 82: // r
+						this.reset();
+						break;
 				}
 
 			});
 
-			if (itemsLeft === 0) {
-				this.state.isWon = true;
-			}
-
 		}
 
-		hasNewStateValue (property) {
-			return this.state[property] !== this.prevState[property];
-		}
+		toggleStateClassname (item, classname) {
 
-		render () {
+			const newClassname ='-is-'+classname;
 
-			const game 		= this;
+			if (classname !== '' && !this.state.classnames.includes(classname)) {
 
-			const state 	= game.state;
-
-			game.evaluate();
-
-			game.selectNode('.status').innerHTML = '';
-
-			if (state.flagged === 0 || game.hasNewStateValue('flagged')) {
-				game.selectNode('.flagged').innerHTML = 'Flagged: '+state.flagged+'/'+game.mineMap.length;
-			}
-
-			if (game.hasNewStateValue('isOn')) {
+				this.state.classnames.push(newClassname);
 
 			}
 
-			if (game.state.isWon) {
-				game.forEachItem((item) => {
-					if (!item.isFlagged) {
-						item.flag();
-					}
-				});
-				game.state.isOn = false;
-				game.selectNode('.status').innerHTML = 'Game Finished!';
-			}
+			this.state.classnames.forEach(oldClassnames => {
 
-			if (game.state.isLost) {
-				game.forEachMine((item) => {
-					if (!item.isCleared) {
-						item.clear('game-over');
-					}
+				if (oldClassnames !== newClassname) {
 
-				});
-				game.state.isOn = false;
-				game.selectNode('.status').innerHTML = 'Game Over!';
-			}
+					item.classList.remove(oldClassnames);
 
-			Object.keys(state).forEach((property) => {
-				this.prevState[property] = state[property];
+				}
+
 			});
 
+			if (classname !== '') {
+
+				item.classList.add(newClassname);
+
+			}
+
 		}
 
-		gameOver () {
-			
-			window.console.log('gameOver');
+		reset (level) {
 
-		}
+			if (level) {
 
-		restart () {
+				if (this.state.levels[level]) {
+
+					this.state.level = level;
+
+					this.state.minesweeperEngine = this.state.minesweeperEngine.create(this.state.levels[level]);
+
+				}
+
+			} else {
+
+				this.state.minesweeperEngine = this.state.minesweeperEngine.reset();
+
+			}
 
 			this.initBoard();
 
 		}
 
-		start () {
+		render () {
 
-			console.log('startGame');
-			this.state.isOn = true;
+			const gameState = this.state.minesweeperEngine;
+
+			const prevLevel = document.querySelector('.controls-level.-is-current');
+
+			const currentLevel = document.querySelector('.controls-level[data-level="'+this.state.level+'"]');
+
+			if (prevLevel) {
+				prevLevel.classList.remove('-is-current');
+			}
+
+			if (currentLevel) {
+				currentLevel.classList.add('-is-current');
+			}
+
+			document.querySelector('.marked').innerHTML 	= gameState.markersUsed+'/'+gameState.markersTotal;
+
+			document.querySelector('.progress').innerHTML 	= gameState.itemsCleared+'/'+gameState.itemsTotal;
+
+			if (gameState.status === 'gameover') {
+
+				document.querySelector('.status').innerHTML = 'Game Over! &#x1f622;';
+
+			} else if (gameState.status === 'completed') {
+
+				document.querySelector('.status').innerHTML = 'Game Completed! &#x1f973; &#x1f389;';
+
+			} else {
+
+				document.querySelector('.status').innerHTML = '&nbsp;';
+
+			}
 
 		}
-
-		timerStop () {}
-
-		resetBoard () {}
-
-		pause () {}
-
-		resume () {}
 
 	}
 
